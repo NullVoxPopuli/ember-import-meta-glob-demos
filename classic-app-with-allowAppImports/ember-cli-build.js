157ce8f34bf650ec0d52ce931c60a75d474f29ba
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-06-03 16:06:12 -0400

    the babel plugin doesn't quite work -- probably due to the same reason it doesn't work in classic

--- classic-app-with-allowAppImports/ember-cli-build.js
+++ classic-app-with-allowAppImports/ember-cli-build.js
@@ -5,6 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    autoImport: {
+      allowAppImports: ['modules/*'],
+    },
+    babel: {
+      plugins: [require.resolve('babel-plugin-transform-vite-meta-glob')],
+    },
   });
 
   return app.toTree();
